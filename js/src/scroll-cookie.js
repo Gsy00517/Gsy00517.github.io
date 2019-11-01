f1c91fd7d385adf18dc7cbbd5254664ce5056bc8
Gsy00517 <917547962@qq.com>
2019-11-01 23:39:47 +0800

    Site updated: 2019-11-01 23:39:45

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Nov 01 2019 20:31:16 GMT+0800 (GMT+08:00)
+// build time:Fri Nov 01 2019 23:39:43 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
