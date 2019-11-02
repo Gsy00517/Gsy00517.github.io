51a660fdbc75f9c0a4e5e57081d2ca9e348cc1c1
Gsy00517 <917547962@qq.com>
2019-11-02 09:57:21 +0800

    Site updated: 2019-11-02 09:57:19

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Nov 02 2019 00:52:22 GMT+0800 (GMT+08:00)
+// build time:Sat Nov 02 2019 09:57:17 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="(＃°Д°)页面崩溃啦~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(●ˇ∀ˇ●)噫又好了~";titleTime=setTimeout(function(){document.title=OriginTitle},1e3)}});
 //rebuild by neat 
