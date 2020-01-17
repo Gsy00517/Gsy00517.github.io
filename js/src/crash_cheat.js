05aa90227013b23b377c768e5c34f344b35ba451
Gsy00517 <917547962@qq.com>
2020-01-17 22:00:13 +0800

    Site updated: 2020-01-17 22:00:11

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 17 2020 10:38:48 GMT+0000 (GMT)
+// build time:Fri Jan 17 2020 22:00:10 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="(＃°Д°)页面崩溃啦~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(●ˇ∀ˇ●)噫又好了~";titleTime=setTimeout(function(){document.title=OriginTitle},1e3)}});
 //rebuild by neat 
