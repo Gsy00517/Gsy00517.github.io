cab066618b6021f9e3f443b2d85aa40725d71159
Gsy00517 <917547962@qq.com>
2020-02-06 19:23:03 +0800

    Site updated: 2020-02-06 19:22:59

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 06 2020 09:03:42 GMT+0800 (GMT+08:00)
+// build time:Thu Feb 06 2020 19:22:58 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
