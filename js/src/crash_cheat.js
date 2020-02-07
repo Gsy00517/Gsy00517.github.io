e572ab0f5624f061c45b58886c4ce4b984137541
Gsy00517 <917547962@qq.com>
2020-02-07 19:27:21 +0800

    Site updated: 2020-02-07 19:27:16

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 07 2020 17:38:25 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 07 2020 19:27:12 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="(＃°Д°)页面崩溃啦~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(●ˇ∀ˇ●)噫又好了~";titleTime=setTimeout(function(){document.title=OriginTitle},1e3)}});
 //rebuild by neat 
--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 07 2020 17:38:25 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 07 2020 19:27:12 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");e();i();function e(){var e=n(),i=f(),o=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight(),a=$("#content").height();if(e+o<a){t.affix({offset:{top:e-CONFIG.sidebar.offset,bottom:i}})}r(e).css({"margin-left":"initial"})}function i(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){o()}})}function n(){return $(".header-inner").height()+CONFIG.sidebar.offset}function f(){var t=$(".footer-inner"),e=t.outerHeight(true)-t.outerHeight(),i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function o(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");e()}});
 //rebuild by neat 
